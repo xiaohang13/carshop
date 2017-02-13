8728e515cb542a16a7acda4bace55c8f52c68f5c
xiaohang <972382409@qq.com>
2017-02-14 01:41:19 +0800

    展示收货地址

--- js/address.js
+++ js/address.js
@@ -1,15 +1,23 @@
 new Vue( {
-    el: "#container",
+    el: ".container",
     data: {
         addressList: []
     },
-    mounted: {
-
+    mounted: function () {
+        this.$nextTick( function () {
+            Vue.config.devtools = true;
+            this.viewAddress();
+        })
     },
     filters: {
 
     },
     methods: {
-
+        // 查看所有地址信息
+        viewAddress: function () {
+            this.$http.get( "data/address.json" ).then( res => {
+                this.addressList = res.body.result;
+            });
+        }
     }
 });
